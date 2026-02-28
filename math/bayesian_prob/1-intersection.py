9c14e7ad9e42ef2125a26086d341cd6e25bf9866
henryparfait <p.uhiriwe@alustudent.com>
2026-02-28 09:21:41 -0800

    Bayesian

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -59,7 +59,7 @@ def intersection(x, n, P, Pr):
         raise TypeError("P must be a 1D numpy.ndarray")
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape "
-                         "as P")
+                        "as P")
     if np.any(P < 0) or np.any(P > 1):
         raise ValueError("All values in P must be in the range [0, 1]")
     if np.any(Pr < 0) or np.any(Pr > 1):
